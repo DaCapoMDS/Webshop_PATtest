978604b2128eecd5b2572bd890974273d6703112
DaCapo <brauneggbenjamin@gmail.com>
2025-01-04 00:37:45 +0100

    fix: Update to production Vercel URL

--- docs/js/modules/constants.js
+++ docs/js/modules/constants.js
@@ -5,7 +5,7 @@ export const GITHUB_CONFIG = {
 };
 
 // API endpoints
-const VERCEL_API_URL = 'https://webshop-pattest-git-main-dacapos-projects.vercel.app';
+const VERCEL_API_URL = 'https://webshop-pattest.vercel.app';
 
 export const API_ENDPOINTS = {
     ISSUES_API: `${VERCEL_API_URL}/api/create-issue`,
